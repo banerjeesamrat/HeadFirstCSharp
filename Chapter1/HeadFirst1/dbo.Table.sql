d39478adf21f61f11c42cf6bb1a6c3946e067811
Samrat Banerjee <banerjeesamrat3@gmail.com>
2018-11-05 23:47:43 +0530

    First Commit
    Need For Speed

---
+++ Chapter1/HeadFirst1/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[People]
+(
+	[ContactId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    	[Name] NVARCHAR(50) NULL,
+	[Company] NVARCHAR(50) NULL,
+	[Telephone] NVARCHAR(50) NULL,
+	[Email] NVARCHAR(50) NULL,
+	[Client] BIT NULL,
+	[LastCall] DATETIME NULL,
+
+)
