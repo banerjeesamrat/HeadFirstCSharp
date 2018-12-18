8c9617bda5d55cf4a30c72b26394e33ce790efe8
Samrat Banerjee <banerjeesamrat3@gmail.com>
2018-12-18 12:05:54 +0530

    Object referencing tricks

---
+++ Swapping/Elephant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Elephant
+{
+    public int EarSize;
+    public string Name;
+
+    public void WhoAmI()
+    {
+        MessageBox.Show("My ears are "+EarSize+" inches tall.",Name+" says..");
+    }
+}
